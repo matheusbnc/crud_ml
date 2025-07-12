9faf38c55d5e97725fd2b41c764755f4c6efb4e1
matheusbnc <matheuschagasbn@gmail.com>
2025-07-12 14:32:58 -0300

    Criando schema.py

---
+++ backend/schema.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel, PositiveFloat, EmailStr, model_validator
+from enum import Enum
+from datetime import datetime
+from typing import Optional
+
+class ProductBase(BaseModel):
+    name: str
+    description: str
+    price: PositiveFloat
+    category: str
+    supplier_email: EmailStr
+
+class ProductCreate(ProductBase):
+    pass
+
+class ProductResponse(ProductBase):
+    id: int
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
+
+class ProductUpdate(BaseModel):
+    name: Optional[str] = None
+    description: Optional[str] = None
+    price: Optional[PositiveFloat] = None
+    category: Optional[str] = None
+    supplier_email: Optional[EmailStr] = None
