02757168b3360903537906c7302125ccd94f6868
matheusbnc <matheuschagasbn@gmail.com>
2025-07-14 10:32:32 -0300

    Adicionando funcao para atualizar produtos

--- backend/crud.py
+++ backend/crud.py
@@ -21,3 +21,27 @@ def delete_product(db: Session, product_id: int):
     db.commit()
     return db_product
 
+def update_product(db: Session, product_id: int, product: ProductUpdate):
+    db_product = db.query(ProductModel).filter(ProductModel.id == product_id).first()
+
+    if db_product is None:
+        return None
+    
+    if product.name is not None:
+        db_product.name = product.name
+    
+    if product.description is not None:
+        db_product.description = product.description
+    
+    if product.price is not None:
+        db_product.price = product.price
+
+    if product.category is not None:
+        db_product.category = product.category
+    
+    if product.supplier_email is not None:
+        db_product.supplier_email = product.supplier_email
+
+    db.commit()
+    db.refresh(db_product)
+    return db_product
