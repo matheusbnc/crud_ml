1aadacf0f885455d27e9e7991d9be98b1910a322
matheusbnc <matheuschagasbn@gmail.com>
2025-07-14 10:58:50 -0300

    router.py: Criando rota para buscar 1 item

--- backend/router.py
+++ backend/router.py
@@ -17,3 +17,10 @@ router = APIRouter()
 def read_all_products(db: Session = Depends(get_db)):
     products = get_product(db)
     return products
+
+@router.get("/product{product_id}", response_model=ProductResponse)
+def read_one_product(product_id: int, db: Session = Depends(get_db)):
+    product = get_product(db, product_id=product_id)
+    if product is None:
+        raise HTTPException(status_code=404, detail="Produto Inexistente")
+
