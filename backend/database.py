d420cb71595e379df49abdf04864809c6a610f83
matheusbnc <matheuschagasbn@gmail.com>
2025-07-14 12:29:29 -0300

    Corrigindo arquivos database.py e docker-compose para nao utilizar mais variavies de ambiente, como forma de teste

--- backend/database.py
+++ backend/database.py
@@ -2,16 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-POSTGRES_USER=os.getenv("POSTGRES_USER")
-POSTGRES_PASSWORD=os.getenv("POSTGRES_PASSWORD")
-POSTGRES_DB=os.getenv("POSTGRES_DB")
-POSTGRES_HOST=os.getenv("POSTGRES_HOST")
-POSTGRES_PORT=os.getenv("POSTGRES_PORT")
+POSTGRES_USER = "postgres"
+POSTGRES_PASSWORD = "postgres"
+POSTGRES_DB = "meubanco"
+POSTGRES_HOST = "postgres"
+POSTGRES_PORT = "5432"
 
 DATABASE_URL = (
     f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}"
@@ -22,7 +17,7 @@ engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base() 
+Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
