07f21457fa2ea5b017b666fccda6eec8c01b3f65
matheusbnc <matheuschagasbn@gmail.com>
2025-07-14 12:44:48 -0300

    router.py: corrigindo rota de criar produto

--- backend/router.py
+++ backend/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from database import SessionLocal, get_db
+from database import get_db
 from schema import ProductResponse, ProductUpdate, ProductCreate
 from typing import List
 from crud import (
@@ -15,7 +15,7 @@ router = APIRouter()
 
 @router.get("/product/", response_model=List[ProductResponse])
 def read_all_products(db: Session = Depends(get_db)):
-    products = get_product(db)
+    products = get_products(db)
     return products
 
 @router.get("/product/{product_id}", response_model=ProductResponse)
@@ -23,10 +23,11 @@ def read_one_product(product_id: int, db: Session = Depends(get_db)):
     product = get_product(product_id=product_id, db=db)
     if product is None:
         raise HTTPException(status_code=404, detail="Produto Inexistente")
+    return product
 
 @router.post("/products/", response_model=ProductResponse)
 def create_product_route(product: ProductCreate, db: Session = Depends(get_db)):
-    return create_product(product, db=db)
+    return create_product(db=db, product=product)
 
 @router.delete("/products/{product_id}", response_model=ProductResponse)
 def delete_product_route(product_id: int, db: Session = Depends(get_db)):
@@ -41,4 +42,3 @@ def update_product_route(product_id: int, product: ProductUpdate, db: Session = 
     if product is None:
         raise HTTPException(status_code=404, detail="Produto Inexistente")
     return product
-    
